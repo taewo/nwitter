4e0abe8f3d7d63d787c5af9704573ad16520ba5e
taewo <moondingie5@gmail.com>
2021-03-28 01:07:52 +0900

    2.6 Log out

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,15 @@
+import { authService } from 'fbase'
+import { useHistory } from 'react-router-dom'
+
 const Profile = () => {
+  const history = useHistory()
+  const onLogOutClick = () => {
+    authService.signOut()
+    history.push('/')
+  }
   return (
     <div>
-      Profile
+      <button onClick={onLogOutClick}>Log out</button>
     </div>
   )
 }
