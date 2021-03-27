48760d7e41e1653e6babf830c315b56f907658bb
taewo <moondingie5@gmail.com>
2021-03-27 23:24:44 +0900

    2.1 Make Login Form

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,32 @@
+import { useState } from 'react'
+
 const Auth = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const onChange = (e) => {
+    const { target: { name, value }}  = e
+    e.preventDefault()
+    console.log(1, name, value)
+    if (name === 'email') {
+      setEmail(value)
+    } else {
+      setPassword(value)
+    }
+  }
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
   return (
     <div>
-      Auth
+      <form onSubmit={onSubmit}>
+        <input type="text" name='email' value={email} placeholder="Email" onChange={onChange} required />
+        <input type="password" name="password" value={password} placeholder="Password" onChange={onChange} required />
+        <input type="submit" value="Log In" />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Githun</button>
+      </div>
     </div>
   )
 }
