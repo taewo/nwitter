c52cdb0f6f4119158187d4501eaa3d6aab976749
taewo <moondingie5@gmail.com>
2021-03-28 00:28:55 +0900

    2.4 Log in

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { authService } from 'fbase'
-import Profile from 'routes/Profile'
 import AppRouter from './Router'
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser)
+  const [init, setInit] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true)
+      } else {
+        setIsLoggedIn(false)
+      }
+      setInit(true)
+    })
+  }, [])
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : 'Loading...'}
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -5,10 +5,10 @@ const Auth = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [newAccount, setNewAccount] = useState(true)
+  const [error, setError] = useState('')
   const onChange = (e) => {
     const { target: { name, value }}  = e
     e.preventDefault()
-    console.log(1, name, value)
     if (name === 'email') {
       setEmail(value)
     } else {
@@ -27,15 +27,21 @@ const Auth = () => {
       console.log(data)
     } catch (e) {
       console.log(e)
+      setError(e.message)
     }
   }
+  const toggleAccount = () => setNewAccount(prev => !prev)
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input type="email" name='email' value={email} placeholder="Email" onChange={onChange} required />
         <input type="password" name="password" value={password} placeholder="Password" onChange={onChange} required />
-        <input type="submit" value={newAccount ? 'Create Account': 'Log in'} />
+        <input type="submit" value={newAccount ? 'Create Account': 'Sign in'} />
+        {error}
       </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? 'Sign in' : 'Create Account'}
+      </span>
       <div>
         <button>Continue with Google</button>
         <button>Continue with Githun</button>
