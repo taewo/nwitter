1265746669e12badc5e2d592061ebb134b1d2197
taewo <moondingie5@gmail.com>
2021-03-27 11:37:11 +0900

    1.0 Remove unused files + Default firebase setting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import firebase from './firebase'
 
 function App() {
+  console.log('firebase--', firebase)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      App
     </div>
   );
 }
