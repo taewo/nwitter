816af11bfde19815311f6859fe00fcb388438c4d
taewo <moondingie5@gmail.com>
2021-03-28 16:10:46 +0900

    3.6 불필요한 state 제거

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,18 @@ import AppRouter from './Router'
 
 function App() {
   const [init, setInit] = useState(false)
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [userObj, setUserObj] = useState(null)
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setIsLoggedIn(true)
         setUserObj(user)
-      } else {
-        setIsLoggedIn(false)
       }
       setInit(true)
     })
   }, [])
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} /> : 'Loading...'}
+      {init ? <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} /> : 'Loading...'}
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
