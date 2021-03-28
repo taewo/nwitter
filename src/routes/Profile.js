dc4cc973ade8ff724d1449c1cd0c522ad33c88a0
taewo <moondingie5@gmail.com>
2021-03-28 22:49:40 +0900

    5.0 dbService에서 nweets 가져오기

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,11 +1,19 @@
-import { authService } from 'fbase'
+import { useEffect } from 'react'
+import { authService, dbService } from 'fbase'
 import { useHistory } from 'react-router-dom'
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const history = useHistory()
+  useEffect(() => {
+    getMyNweets()
+  }, [])
   const onLogOutClick = () => {
     authService.signOut()
     history.push('/')
+  }
+  const getMyNweets = async () => {
+    const nweets = await dbService.collection('nweets').where('creatorId', '==', userObj.uid).orderBy('createdAt').get()
+    console.log(nweets.docs.map(doc => doc.data()))
   }
   return (
     <div>
