0e2f22f29b196aa7c37e3e61c6bff0baaaad28c5
taewo <moondingie5@gmail.com>
2021-03-28 17:18:04 +0900

    4.2-2 userObj.uid폴더에 uuidv4()이름으로 이미지 업로드

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,13 @@
 import { useState, useEffect } from 'react'
-import { dbService } from 'fbase'
+import { dbService, storageService } from 'fbase'
+import { v4 as uuidv4 } from "uuid";
 import Nweet from 'components/Nweet'
 
 const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState('')
   const [nweets, setNweets] = useState([])
+  const [attachment, setAttachment] = useState(null)
+
   useEffect(() => {
     dbService.collection('nweets').onSnapshot((snapshot) => {
       const nweetArray = snapshot.docs.map(doc => {
@@ -24,19 +27,42 @@ const Home = ({ userObj }) => {
 
   const onSubmit = async (e) => {
     e.preventDefault()
-    const result = await dbService.collection('nweets').add({
-      text: nweet,
-      createdAt: Date.now(),
-      creatorId: userObj.uid
-    })
-    setNweet('')
+    const fileRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`)
+    const response = await fileRef.putString(attachment, 'data_url')
+    console.log('response-' ,response)
+    // const result = await dbService.collection('nweets').add({
+    //   text: nweet,
+    //   createdAt: Date.now(),
+    //   creatorId: userObj.uid
+    // })
+    // setNweet('')
   }
+
+  const onFileChange = (e) => {
+    const { target: { files } } = e
+    const theFile = files[0]
+    const reader = new FileReader()
+    reader.readAsDataURL(theFile)
+    reader.onloadend = (finishedEvent) => {
+      const { currentTarget: { result } } = finishedEvent
+      setAttachment(result)
+    }
+  }
+
+  const onClearAttachment = () => setAttachment(null)
 
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input type="text" placeholder="What's on your mind" maxLength={120} value={nweet} onChange={onChange} />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="Nweet" />
+        {attachment && (
+          <div>
+            <img src={attachment} width="50px" height="50px" />
+            <button onClick={onClearAttachment}>Clear</button>
+          </div>
+        )}
       </form>
       <div>
         {nweets.map((nweet) => (
