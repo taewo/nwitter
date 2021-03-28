3f0fed52968d58203fe6926757525fd7dfa6b74a
taewo <moondingie5@gmail.com>
2021-03-28 15:45:09 +0900

    3.5-2 Nweet 텍스트 수정기능 구현

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,22 +1,51 @@
+import { useState } from 'react'
 import { dbService } from 'fbase'
 
 const Nweet = ({ nweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false)
+  const [newNweet, setNewNweet] = useState(nweetObj.text)
+
   const onDeleteClick = async () => {
     const ok = window.confirm('Are you sure you want to delete this nweet?')
     if (ok) {
       await dbService.doc(`nweets/${nweetObj.id}`).delete()
     }
   }
-
+  const toggleEditing = () => setEditing(prev => !prev)
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    console.log(111, newNweet)
+    await dbService.doc(`nweets/${nweetObj.id}`).update({
+      text: newNweet
+    })
+    toggleEditing()
+  }
+  const onChange = (e) => {
+    const { target: { value }} = e
+    setNewNweet(value)
+  }
   return (
     <div>
-      <h4>{nweetObj.text}</h4>
-      {isOwner && (
+      {editing ? (
         <>
-          <button onClick={onDeleteClick}>Delete Nweet</button>
-          <button>Edit Nweet</button>
+          <form onSubmit={onSubmit}>
+            <input onChange={onChange} type="text" placeholder="Edit your nweet" value={newNweet} required />
+            <input type="submit" value="Update Nweet" />
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
         </>
-      )}
+      ) : (
+        <>
+          <h4>{nweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete Nweet</button>
+              <button onClick={toggleEditing}>Edit Nweet</button>
+            </>
+          )}
+        </>
+        )
+      }
     </div>
   )
 }
